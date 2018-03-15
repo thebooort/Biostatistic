d33e5c16f87df80f89571e508ccf01a668d3890b
Bartolomé Ortiz Viso <thebart94@hotmail.com>
2018-03-15 09:52:41 +0100

    added comments, code needed

--- aceptacion_rechazo_var_continua.m
+++ aceptacion_rechazo_var_continua.m
@@ -1,1 +1,13 @@
 #tecnica de aceptacion rechazo para variables continuas
+
+#PASO PREVIO generacion del valor de C
+
+
+
+#PASO 1:Generar un valor y 0 de la variable Y con función de densidad g Y , mediante el método
+#de la Transformada Inversa.
+
+#PASO 2: Generar un número aleatorio uniforme u.
+
+#PASO 3:Si se verifica la desigualdad u <= f_X(y _0 )/c*g_Y(y_0).
+#Si no pues volvemos al paso uno, si s´i pues aceptamos el valor.
